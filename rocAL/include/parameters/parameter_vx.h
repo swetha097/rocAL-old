dcb840cbee30aad4d284b410d7adf9539b0c9a31
Swetha B S <swetha@multicorewareinc.com>
2023-11-22 12:00:32 +0000

    rocAL Parameter updation

--- rocAL/include/parameters/parameter_vx.h
+++ rocAL/include/parameters/parameter_vx.h
@@ -52,11 +52,10 @@ class ParameterVX {
             THROW("Reading vx scalar failed" + TOSTR(status));
     }
     void create_array(std::shared_ptr<Graph> graph, vx_enum data_type, unsigned batch_size) {
-        // _arrVal = (T*)malloc(sizeof(T) * _batch_size);
         _batch_size = batch_size;
-        _arrVal.resize(_batch_size);
+        _param->create_array(_batch_size);
         _array = vxCreateArray(vxGetContext((vx_reference)graph->get()), data_type, _batch_size);
-        vxAddArrayItems(_array, _batch_size, _arrVal.data(), sizeof(T));
+        vxAddArrayItems(_array, _batch_size, get_renewed_array().data(), sizeof(T));
         update_array();
     }
     void create_tensor(std::shared_ptr<Graph> graph, TensorInfo info) {
@@ -74,10 +73,9 @@ class ParameterVX {
         _batch_size = batch_size;
         vx_size output_dims[1];
         size_t num_tensor_dims;
-        _arrVal.resize(batch_size);
-        vx_size stride_output[1] = {sizeof(_arrVal[0])};
-        std::cerr << "stride_output :: " << stride_output[0] << "\t sizeof(_arrVal) : " << sizeof(_arrVal) << "\n sizeof(_arrVal[0]) : " << sizeof(_arrVal[0]);
-        _tensor = vxCreateTensorFromHandle(vxGetContext((vx_reference)graph->get()), 1, dims, data_type, 0, stride_output, _arrVal.data(), VX_MEMORY_TYPE_HOST);
+        _param->create_array(batch_size);
+        vx_size stride_output[1] = { sizeof(_arrVal[0]) };
+        _tensor = vxCreateTensorFromHandle(vxGetContext((vx_reference)graph->get()), 1, dims, data_type, 0, stride_output, get_renewed_array().data(), VX_MEMORY_TYPE_HOST);
         update_tensor();
     }
     void set_tensor(vx_tensor external_source_tensor) {
@@ -128,33 +126,20 @@ class ParameterVX {
     }
     void update_array() {
         vx_status status;
-        for (uint i = 0; i < _batch_size; i++) {
-            _arrVal[i] = renew();
-            std::cerr << "\n _arrVal[i] : " << _arrVal[i];
-            // INFO("update_array: " + TOSTR(i) + "," + TOSTR(_arrVal[i]));
-        }
-        status = vxCopyArrayRange((vx_array)_array, 0, _batch_size, sizeof(T), _arrVal.data(), VX_WRITE_ONLY, VX_MEMORY_TYPE_HOST);
+        status = vxCopyArrayRange((vx_array)_array, 0, _batch_size, sizeof(T), get_renewed_array().data(), VX_WRITE_ONLY, VX_MEMORY_TYPE_HOST);
         if (status != 0)
             THROW(" vxCopyArrayRange failed in update_array (ParameterVX): " + TOSTR(status))
     }
     
     void update_tensor(TensorInfo info) {
         vx_status status;
-        for (uint i = 0; i < _batch_size; i++) { 
-            _arrVal[i] = renew();
-            std::cerr << "\n _arrVal[i] : " << _arrVal[i];
-        }
-        status = vxCopyTensorPatch((vx_tensor)_tensor, info.num_of_dims(), nullptr, nullptr, info.strides(), _arrVal.data(), VX_WRITE_ONLY, VX_MEMORY_TYPE_HOST);
+        status = vxCopyTensorPatch((vx_tensor)_tensor, info.num_of_dims(), nullptr, nullptr, info.strides(), get_renewed_array().data(), VX_WRITE_ONLY, VX_MEMORY_TYPE_HOST);
         if (status != VX_SUCCESS)
             THROW("ERROR: vxCopyTensorPatch: write failed" + TOSTR(status));
     }
 
     void update_tensor() {
         vx_status status;
-        for (uint i = 0; i < _batch_size; i++) { 
-            _arrVal[i] = renew();
-            std::cerr << "\n _arrVal[i] : " << _arrVal[i];
-        }
         vx_size stride_output[1] = {sizeof(float)};
         vx_size output_dims[1];
     }
@@ -183,6 +168,11 @@ vx_enum interpret_tensor_data_type(RocalTensorDataType data_type) {
         _param->renew();
         return _param->get();
     }
+
+    std::vector<T> get_renewed_array() {
+        return _param->get_array();
+    }
+
     ~ParameterVX() {
         if(_tensor)
             vxReleaseTensor((vx_tensor*)&_tensor);
